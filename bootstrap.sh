257e27847f8be21afbe4e280db90ae8d222183ed
Nicolò Diamante <hello@nicolodiamante.com>
2023-08-16 12:58:34 +0200

    improved installation process, added K8s env in the prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
+#!/bin/zsh
 
 # Determines the current user's shell.
-[[ "$(basename -- "$SHELL")" == "zsh" ]] || exit 1
+[[ "$SHELL" == */zsh ]] || exit 1
 
-SOURCE=https://github.com/nicolodiamante/simple
+SOURCE="https://github.com/nicolodiamante/simple"
 TARBALL="${SOURCE}/tarball/master"
 TARGET="${HOME}/simple"
-TAR_CMD="tar -xzv -C "${TARGET}" --strip-components 1 --exclude .gitignore"
+TAR_CMD="tar -xzv -C \"$TARGET\" --strip-components 1 --exclude .gitignore"
 
 INSTALL=./utils/install.sh
 
 is_executable() {
-  type "$1" > /dev/null 2>&1
+  command -v "$1" > /dev/null 2>&1
 }
 
 # Checks which executable is available then downloads and installs.
@@ -27,5 +27,10 @@ if [[ -z "$CMD" ]]; then
   echo 'No git, curl or wget available. Aborting!'
 else
   echo 'Installing simple prompt...'
-  mkdir -p "${TARGET}" && eval "${CMD}" && cd "${TARGET}" && source "${INSTALL}"
+  mkdir -p "$TARGET"
+    if eval "$CMD"; then
+    cd "$TARGET" && source "$INSTALL"
+  else
+    echo "Installation failed. Aborting!"
+  fi
 fi
--- utils/install.sh
+++ utils/install.sh
@@ -1,24 +1,27 @@
-#!/bin/sh
+#!/bin/zsh
 
 #
-# Install simple
+# Install Simple
 #
 
-# Determines the current user's shell, if `zsh` then installs.
-[[ "$(basename -- "$SHELL")" == "zsh" ]] || exit 1
+# Determines the current user's shell, if not `zsh` then exit.
+if [[ "$(basename -- "$SHELL")" != "zsh" ]]; then
+  echo "Please switch to zsh shell to continue."
+  exit 1
+fi
 
 # Defines the PATHs.
 SCRIPT="${HOME}/simple/script"
 ZSHRC="${ZDOTDIR:-${XDG_CONFIG_HOME/zsh:-$HOME}}/.zshrc"
 
 if [[ -d "$SCRIPT" && -f "$ZSHRC" ]]; then
-  # Appends to `zshrc`.
-  cat << EOF >> ${ZSHRC}
+# Append the necessary lines to zshrc.
+cat << EOF >> ${ZSHRC}
 # simple
 fpath=(~/simple/script \$fpath)
 autoload -Uz simple
 EOF
-  echo 'zsh: appended simple to zshrc.'
+  echo 'zsh: append Simple's necessary lines to .zshrc'
 
   # Reloads shell.
   source "${ZSHRC}"
